a3bb29abaafe6defdf81cd6846b8da626ac753ad
Tim Repke <repke@mcc-berlin.net>
2024-12-19 13:59:37 +0000

    fix types

--- shared/types.py
+++ shared/types.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 
-type IntList = np.ndarray[tuple[int], np.dtype[np.int_]] | list[int] | pd.Series[int]
-type FloatList = np.ndarray[tuple[float], np.dtype[np.int_]] | list[float] | pd.Series[float]
-type StrList = list[str] | pd.Series[str]
+type IntList = np.ndarray[tuple[int], np.dtype[np.int_]] | list[int] | pd.Series
+type FloatList = np.ndarray[tuple[float], np.dtype[np.int_]] | list[float] | pd.Series
+type StrList = list[str] | pd.Series
 
 
 __all__ = ['IntList', 'FloatList', 'StrList']
