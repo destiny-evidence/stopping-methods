814f40bf77a7e418b5e551dc7861d8bbc5cc6c70
L-ENA <lena.schmidt.0493@gmail.com>
2025-01-13 13:43:10 +0000

    Adding heuristic fixed N
    Adding heuristic fixed N to init file

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,5 +1,6 @@
 from .buscar import Buscar
 from .alison import Alison
 from .heuristic_fractions import HeuristicFraction
+from .heuristic_fixed_N import HeuristicFixedN
 
-__all__ = ['Buscar', 'Alison', 'HeuristicFraction']
+__all__ = ['Buscar', 'Alison', 'HeuristicFraction', 'HeuristicFixedN']
