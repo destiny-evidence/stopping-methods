f4931b839d537e6a68603738b416db8d0e5436e7
Tim Repke <repke@mcc-berlin.net>
2025-06-19 16:42:07 +0200

    clean quantci

--- methods/quant_ci.py
+++ methods/quant_ci.py
@@ -1,13 +1,8 @@
-import os,sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 import logging
-from pathlib import Path
 from typing import Generator, TypedDict
 import numpy as np
 import pandas as pd
 
-from shared.dataset import RankedDataset
 from shared.method import AbstractMethod, AbstractLogEntry, RECALL_TARGETS
 from shared.types import IntList, FloatList
 
@@ -26,7 +21,7 @@ class QuantCIParamSet(TypedDict):
 
 
 class QuantCILogEntry(AbstractLogEntry):
-    KEY: str = 'QUANT'
+    KEY: str = 'QUANT_CI'
     recall_target: float
     nstd: float
     est_recall: float
@@ -34,7 +29,7 @@ class QuantCILogEntry(AbstractLogEntry):
 
 
 class QuantCI(AbstractMethod):
-    KEY: str = 'QUANT'
+    KEY: str = 'QUANT_CI'
 
     def parameter_options(self) -> Generator[QuantCIParamSet, None, None]:
         for recall_target in RECALL_TARGETS:
@@ -48,7 +43,16 @@ class QuantCI(AbstractMethod):
                 recall_target: float = 0.9,
                 nstd: float = 0) -> QuantCILogEntry:
 
-        if len(list_of_labels) < 2:
+        scores_all = np.array(list_of_model_scores)
+        labels = np.array(list_of_labels)
+
+        # mask nans and infs in scores
+        mask = np.isfinite(scores_all)
+        scores = scores_all[mask]
+        labels = labels[mask[:len(labels)]]
+
+        # return early if not enough labels are left
+        if len(list_of_labels) < 50:
             return QuantCILogEntry(
                 safe_to_stop=False,
                 recall_target=recall_target,
@@ -56,25 +60,14 @@ class QuantCI(AbstractMethod):
                 est_recall=0.0
             )
 
-        scores_all = np.array(list_of_model_scores)
-        labels = np.array(list_of_labels)
-
-        # mask nans and infs in scores
-        mask = ~np.isfinite(scores_all)
-        scores = np.ma.array(scores_all, mask=mask)
-
-        # `ps` stands for probability sum
+        # calculate probability sums
         known_ps = scores[:len(labels)].sum()
         unknown_ps = scores[len(labels):].sum()
 
-        if known_ps is np.ma.masked or unknown_ps is np.ma.masked:
-            est_recall = np.nan
-        elif (known_ps + unknown_ps) <= 0:
-            est_recall = 0
-        else:
-            est_recall = known_ps / (known_ps + unknown_ps)
+        est_recall = known_ps / (known_ps + unknown_ps) if (known_ps + unknown_ps) > 0 else 0
 
         if nstd == 0:
+            # this is effectively the QUANT method (without CI)
             return QuantCILogEntry(
                 safe_to_stop=est_recall >= recall_target,
                 recall_target=recall_target,
@@ -87,12 +80,8 @@ class QuantCI(AbstractMethod):
         unknown_var = prod[len(labels):].sum()
         est_var = ((known_ps ** 2 / (known_ps + unknown_ps) ** 4 * all_var) +
                    (1 / (known_ps + unknown_ps) ** 2 * (all_var - unknown_var)))
-        if est_var is np.ma.masked:
-            est_var = np.nan
-            safe_to_stop = False
-        else:
-            safe_to_stop = est_recall - nstd * np.sqrt(est_var) >= recall_target
-        
+        safe_to_stop = (est_recall - nstd * np.sqrt(est_var)) >= recall_target
+
         return QuantCILogEntry(
             safe_to_stop=safe_to_stop,
             recall_target=recall_target,
@@ -102,60 +91,22 @@ class QuantCI(AbstractMethod):
         )
 
 
-def test(ranking: Path,
-         batch_size: int = 25):
-    dataset = RankedDataset(ranking)
-    batches, labels, scores, is_prioritised = dataset.data
-    labels = np.array(labels)
-    data = np.array([np.arange(len(labels)), labels.cumsum()]).T
-    # data = MinMaxScaler().fit_transform(data)
-    recall_target = 0.9
-    stopper = QuantCI(dataset)
-    xs = []
-    log = []
-    for n_seen in range(0, dataset.n_total, batch_size):
-        logger.info(f'Running batch for items {n_seen:,}–{n_seen + batch_size:,}')
-
-        batch_labels = labels[n_seen:n_seen + batch_size]
-        score = stopper.compute(
-            list_of_labels=batch_labels,
-            list_of_model_scores=np.array(scores),
-            is_prioritised=is_prioritised[n_seen:n_seen + batch_size],
-            recall_target=recall_target,
-            nstd=0.1,
-        )
-        logger.info(f' > score: {score}')
-        log.append(score)
-        xs.append(n_seen + batch_size)
-
-    fig, ax = plt.subplots()
-    ax.plot(data[:, 0] / dataset.n_total, data[:, 1], label='include', ls='-')
-    ax2 = ax.twinx()
-    ax2.plot(np.array(xs) / dataset.n_total, [l.est_var for l in log], label='est_var')
-    ax2.plot(np.array(xs) / dataset.n_total, [l.est_recall for l in log], label='est_recall')
-    # ax2.hlines(recall_target, 0, 1, lw=1)
-    fig.legend()
-    fig.show()
-
-
-
 if __name__ == '__main__':
+    import os
+    import sys
     from shared.test import test_method, plots
 
-    dataset, results = test_method(QuantCI, QuantCIParamSet(recall_target=0.8, nstd=2), 4)
-    fig, ax = plots(dataset, results)
-    fig.savefig('data/plots/method_quant_viz.png')
-    logger.debug(f'estimated recall: {[res['est_recall'] for res in results]}')
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    bs = 15
+    dataset, results = test_method(QuantCI, QuantCIParamSet(recall_target=0.8, nstd=2),
+                                   dataset_i=4, batch_size=bs)
+    est_recalls = np.array([res['est_recall'] for res in results])
 
-# if __name__ == '__main__':
-#     import typer
-#     from matplotlib import pyplot as plt
-#     from sklearn.preprocessing import MinMaxScaler
-#
-#     typer.run(test)
-#
-#     # print(data.shape)
-#     # knees = detect_knee(data, window_size=1, s=1)
-#     # data = np.array([np.arange(len(labels)) / len(labels),
-#     #                  labels.cumsum() / labels.sum()]).T[:3000]
-#     # knees = detect_knee(data[::500], window_size=1, s=100)
+    fig, ax = plots(dataset, results)
+    logger.debug(f'estimated recall: {[res['est_recall'] for res in results]}')
+    ax2 = ax.twinx()
+    ax2.set_ylim([0, 1])
+    ax.plot(np.arange(len(results)) * bs, est_recalls * dataset.n_incl, label='est_recall*n_incl')
+    ax2.plot(np.arange(len(results)) * bs, est_recalls, label='est_recall')
+    # fig.savefig('data/plots/method_quant_viz.png')
+    fig.show()
